ca155a51dd4ffcebce84f7936e7bd10203d69cb8
Alessandro Poletti <pole97@live.it>
2026-01-13 16:43:23 +0100

    minor structural change index.ts - routes.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,6 @@
 const cors = require("cors");
 import express, { Application, Request, Response } from "express";
 import initRoutes from "./routes";
-const morgan = require("morgan");
 
 const app: Application = express();
 const PORT: number = 3001;
@@ -12,8 +11,6 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.use(morgan("dev"));
-app.use(express.json());
 initRoutes(app);
 
 if (require.main === module) {
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import { Application, Request, Response } from "express";
 import DocumentRoutes from "./routes/documentRoutes";
+const morgan = require("morgan");
 
 function initRoutes(app: Application): void {
+  app.use(morgan("dev"));
   app.use(express.json());
 
   const documentRoutes = new DocumentRoutes();
